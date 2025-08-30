e1b30fc6d5ed47ee1fba24453c40b54b9101e3b2
RizkyPratama05 <rizkypp428@gmail.com>
2025-08-30 17:11:38 +0700

    pull segera

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3307,
 });
 
 console.log('DB_USER:', process.env.DB_USER);
